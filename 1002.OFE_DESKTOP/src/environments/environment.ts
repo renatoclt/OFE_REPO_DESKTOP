0cb54523f683b690e8a242fe6cc9d7f8678da59e
renatoclt <renatoclt@hotmail.com>
2018-06-22 19:19:07 -0500

    cambio de ambiente

--- 1002.OFE_DESKTOP/src/environments/environment.ts
+++ 1002.OFE_DESKTOP/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  BASE_URL: 'https://qa.ebizlatindata.com/',
-  OCP_APIM_SUBSCRIPTION_KEY:"71d24c28e69d4528ad198666e52504ff",
+  BASE_URL: 'https://dev.ebizlatindata.com/', 
+  OCP_APIM_SUBSCRIPTION_KEY:"07a12d074c714f62ab037bb2f88e30d3"  ,
   ID_ENTIDAD: '1'
 };
